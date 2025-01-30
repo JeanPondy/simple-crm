189ccd9bc8ea3300dcdc10bbb8d41c3f34c96847
Jean Pondy <manbayesse@gmail.com>
2025-01-30 02:24:09 +0100

    routing zu components

--- simple-crm/src/app/app.component.ts
+++ simple-crm/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ MatToolbarModule, MatIconModule, MatSidenavModule],
+  imports: [ MatToolbarModule, MatIconModule, MatSidenavModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- simple-crm/src/app/app.routes.ts
+++ simple-crm/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{ path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Startseite auf /dashboard weiterleiten
+    { path: '', component: DashboardComponent }, // Startseite
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'user', component: UserComponent }
+];
