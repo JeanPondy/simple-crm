0408e074c8f9cd08d6e84bd1172d1e224f448675
Jean Pondy <manbayesse@gmail.com>
2025-01-30 02:48:58 +0100

    add user button

--- simple-crm/src/app/app.component.ts
+++ simple-crm/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { UserComponent } from './user/user.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @Component({
   selector: 'app-root',
--- simple-crm/src/app/user/user.component.ts
+++ simple-crm/src/app/user/user.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
