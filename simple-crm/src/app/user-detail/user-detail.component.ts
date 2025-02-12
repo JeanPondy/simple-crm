815177e8a690a18ee223c4ca8a36bfa44184ebfd
Jean Pondy <manbayesse@gmail.com>
2025-02-12 04:44:24 +0100

    User Detail - Hearder

--- simple-crm/src/app/user-detail/user-detail.component.ts
+++ simple-crm/src/app/user-detail/user-detail.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatCardModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
