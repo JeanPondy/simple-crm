1768b3af325d169df4a3b3340634fc131bbdbda6
Jean Pondy <manbayesse@gmail.com>
2025-02-17 05:12:51 +0100

    Die Fehlermeldung Incomplete: No specs found, , randomized with seed 14878

--- simple-crm/src/app/app.component.spec.ts
+++ simple-crm/src/app/app.component.spec.ts
@@ -2,13 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from '@environments/environment';
+import { environment } from '../environments/environments'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing'; 
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         AppComponent,
+        BrowserAnimationsModule,
+        RouterTestingModule  
       ],
       providers: [
         provideFirebaseApp(() => initializeApp(environment.firebase)),
